e7b6a9b3218ea140b5bbc1c647729df738716b02
Eric Chung <rchung.eric@gmail.com>
2018-01-10 02:41:56 +1100

    added route to PostsNew Component

---
+++ src/components/posts_new.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>PostsNew!</div>
+      // <form className="input-group">
+      //   <input />
+      //   <input />
+      //   <input />
+      // </form>
+    );
+  }
+}
+
+
+
+export default PostsNew;
