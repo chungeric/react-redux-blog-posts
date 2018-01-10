95a1865f64566eac41210806d1ece10dd4bbbd1f
Eric Chung <rchung.eric@gmail.com>
2018-01-11 10:55:04 +1100

    added redux-form and created Input Fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 
 
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+      <label>{field.label}</label>
+        <input
+          // JSX- field.input is an object. ... allows us to access its propreties in the component's props
+          {...field.input}
+          type="text"
+          className="form-control"
+        />
+      </div>
+    )
+  }
+
+
+
   render() {
     return (
-      <div>PostsNew!</div>
-      // <form className="input-group">
-      //   <input />
-      //   <input />
-      //   <input />
-      // </form>
+      <div>
+        <form>
+          <Field
+            name="title"
+            label="Title"
+            component={this.renderField}
+          />
+          <Field
+            name="tags"
+            label="Tags"
+            component={this.renderField}
+          />
+          <Field
+            name="content"
+            label="Post Content"
+            component={this.renderField}
+          />
+        </form>
+      </div>
     );
   }
 }
 
 
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
