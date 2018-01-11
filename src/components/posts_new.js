a46608c33a27b9b6807a03d90cfe5175e8e372dc
Eric Chung <rchung.eric@gmail.com>
2018-01-11 11:26:47 +1100

    added form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -6,33 +6,44 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
-      <label>{field.label}</label>
+      <div className={className}>
+        <label>{field.label}</label>
         <input
           // JSX- field.input is an object. ... allows us to access its propreties in the component's props
           {...field.input}
           type="text"
           className="form-control"
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
-
+  onSubmit(values) {
+    console.log(values);
+  }
 
   render() {
+    // after form is 'handled' by redux-form, it then calls our own defined function
+    const { handleSubmit } = this.props;
+
     return (
       <div>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
             name="title"
             label="Title"
             component={this.renderField}
           />
           <Field
-            name="tags"
-            label="Tags"
+            name="categories"
+            label="Categories"
             component={this.renderField}
           />
           <Field
@@ -40,6 +51,7 @@ class PostsNew extends Component {
             label="Post Content"
             component={this.renderField}
           />
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
@@ -47,7 +59,34 @@ class PostsNew extends Component {
 }
 
 
+// called by **redux-form** automatically during form's life cycle
+function validate(values) {
 
+  // console.log(values) -> { title: 'aaa', categories: 'aaa', content:'aaa' }
+  const errors = {};
+
+  // Validate inputs from values object
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  // If errors is empty, reduxForm assumes form is ok to submit
+  // If errors has any propertis, redux form assumes form is invalid
+  return errors;
+}
+
+
+// reduxForm here, adds handleSubmit and other functions
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
+
+
+// redux form field states -> pristine(init), touched, invalid
